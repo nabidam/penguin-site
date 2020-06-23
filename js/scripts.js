280e82ec658922aba14000ac493ef9393e6f0152
Navid <navidabbaspoor@gmail.com>
2020-06-23 21:40:50 +0430

    landing

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".products-carousel").owlCarousel({
+    margin: 20,
+    mouseDrag: false,
+    rtl: true,
+  });
+
+  $(".others-carousel").owlCarousel({
+    items: 1,
+    dots: true,
+    autoplay: true,
+    rtl: true,
+  });
+});
